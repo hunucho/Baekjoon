cf49cd82df72ed841eb27a8749d411b2ef55305c
hunucho <hunu_cho@naver.com>
2022-01-09 01:35:52 +0900

    20220109
    experiment

---
+++ Baekjoon 2558/Main_B5_2558.java
@@ -0,0 +1,16 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_B5_2558 {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		System.out.println(a+b);
+	}
+}
