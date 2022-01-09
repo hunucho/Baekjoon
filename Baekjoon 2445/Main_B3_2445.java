f1cbda851ffacf592c4029c7610b9ce6a0498365
hunucho <hunu_cho@naver.com>
2022-01-09 20:33:38 +0900

    20220109

---
+++ Baekjoon 2445/Main_B3_2445.java
@@ -0,0 +1,35 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_B3_2445 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<i+1;j++)
+				System.out.print("*");
+			for(int j=0;j<n-i-1;j++)
+				System.out.print("  ");
+			for(int j=0;j<i+1;j++)
+				System.out.print("*");
+			System.out.println("");
+		}
+		
+		for(int i=0;i<n-1;i++) {
+			for(int j=0;j<n-i-1;j++) {
+				System.out.print("*");
+			}
+			for(int j=0;j<i+1;j++){
+				System.out.print("  ");
+			}
+			for(int j=0;j<n-i-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
