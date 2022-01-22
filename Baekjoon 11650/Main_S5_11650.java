5166ee1dd2ebfd1c79569576adaa62162bd67861
hunucho <hunu_cho@naver.com>
2022-01-23 01:00:26 +0900

    20220123

---
+++ Baekjoon 11650/Main_S5_11650.java
@@ -0,0 +1,40 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main_S5_11650 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st =  new StringTokenizer(br.readLine());
+		int n=Integer.parseInt(st.nextToken());
+		ArrayList<ArrayList<Integer>> arr = new ArrayList<ArrayList<Integer>>();
+		
+		for(int i=0;i<n;i++) {
+			arr.add(new ArrayList<>());
+			st = new StringTokenizer(br.readLine());
+			arr.get(i).add(Integer.parseInt(st.nextToken()));
+			arr.get(i).add(Integer.parseInt(st.nextToken()));
+		}
+					
+		Collections.sort(arr, new Comparator<ArrayList<Integer>>() {
+			@Override
+			public int compare(ArrayList<Integer> o1, ArrayList<Integer> o2) {
+//				System.out.println(o1+", "+o2);
+				if(o1.get(0) == o2.get(0))
+					return o1.get(1).compareTo(o2.get(1));
+				else
+					return o1.get(0).compareTo(o2.get(0));
+			}
+		});
+				
+		for(int i=0;i<n;i++)
+			System.out.println(arr.get(i).get(0)+" "+arr.get(i).get(1));
+		
+	}
+}
