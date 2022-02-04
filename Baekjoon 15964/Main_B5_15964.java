1340caabeacfc4a1e9c0bca0e03998bcb68b3496
hunucho <hunu_cho@naver.com>
2022-02-04 13:45:12 +0900

    20220204

---
+++ Baekjoon 15964/Main_B5_15964.java
@@ -0,0 +1,13 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Main_B5_15964 {
+
+	public static void main(String[] args) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		long a=sc.nextLong(), b=sc.nextLong();
+		System.out.println((a+b)*(a-b));
+	}
+
+}
