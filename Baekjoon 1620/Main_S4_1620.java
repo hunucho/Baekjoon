bc4a54bbabc6b3a9c1870f51cda2bf68617b0cab
hunucho <hunu_cho@naver.com>
2022-02-14 01:11:01 +0900

    20220214

---
+++ Baekjoon 1620/Main_S4_1620.java
@@ -0,0 +1,35 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+
+public class Main_S4_1620 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n=Integer.parseInt(br.readLine());
+		HashMap<String, Integer> names = new HashMap<>();
+		String str;
+		for(int i=0;i<n;i++) {
+			str=br.readLine();
+			if(names.containsKey(str))
+				names.put(str, names.get(str)+1);
+			else
+				names.put(str, 1);
+		}
+		for(int i=0;i<n-1;i++) {
+			str=br.readLine();
+			if(names.get(str)==1)
+				names.remove(str);
+			else
+				names.put(str, names.get(str)-1);
+		}
+		String[] arr = names.keySet().toArray(new String[names.size()]);
+		bw.write(arr[0]);
+		bw.close();
+	}
+}
