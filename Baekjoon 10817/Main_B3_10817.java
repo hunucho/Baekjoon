b2aae8c6337f5c4d4338099a683dbc0394b1a3c7
hunucho <hunu_cho@naver.com>
2022-02-20 23:18:38 +0900

    20220220

---
+++ Baekjoon 10817/Main_B3_10817.java
@@ -0,0 +1,15 @@
+package bronze;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main_B3_10817 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] a = new int[3];
+		for (int i = 0; i < 3; i++)
+			a[i] = sc.nextInt();
+		Arrays.sort(a);
+		System.out.println(a[1]);
+	}
+}
