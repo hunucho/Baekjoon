e5482a85af38cf6ea299076837c6fc1081c0b600
hunucho <hunu_cho@naver.com>
2022-03-06 17:28:35 +0900

    20220306

---
+++ Baekjoon 16953/Main_S1_16953.java
@@ -0,0 +1,24 @@
+package silver;
+
+import java.util.Scanner;
+
+public class Main_S1_16953 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt(), b = sc.nextInt(), cnt=1;		
+		while (b>=a) {
+			if(b==a) {
+				System.out.println(cnt);
+				return;
+			}
+			if(b%10==1)
+				b/=10;
+			else if(b%2==0)
+				b/=2;
+			else
+				break;
+			cnt++;
+		}
+		System.out.println(-1);
+	}
+}
