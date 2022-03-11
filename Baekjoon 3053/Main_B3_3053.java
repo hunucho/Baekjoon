d9f33c1b633991a3f97125d8e0f00108e2b89058
hunucho <hunu_cho@naver.com>
2022-03-11 11:04:24 +0900

    20220311

---
+++ Baekjoon 3053/Main_B3_3053.java
@@ -0,0 +1,11 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Main_B3_3053 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int r = sc.nextInt();
+		System.out.println(Math.PI*r*r+"\n"+(double)r*r*2);
+	}
+}
