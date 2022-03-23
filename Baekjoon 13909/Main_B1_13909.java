340899d7c8607f38efecd55c473b53ce29eb6105
hunucho <hunu_cho@naver.com>
2022-03-23 14:54:56 +0900

    20220323

---
+++ Baekjoon 13909/Main_B1_13909.java
@@ -0,0 +1,11 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Main_B1_13909 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println((int)Math.floor(Math.sqrt(n)));			
+	}
+}
