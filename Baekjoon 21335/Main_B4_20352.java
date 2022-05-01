63e5132ddf6994889c3c95177389ad8dd468b346
hunucho <hunu_cho@naver.com>
2022-05-01 23:45:56 +0900

    20220501

---
+++ Baekjoon 21335/Main_B4_20352.java
@@ -0,0 +1,10 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Main_B4_20352 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(2*Math.pow(sc.nextDouble()*Math.PI, 0.5));
+	}
+}
