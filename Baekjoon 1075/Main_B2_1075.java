68f9583cf1b341213c763dd8f0284df3d8773366
hunucho <hunu_cho@naver.com>
2022-05-13 23:16:42 +0900

    20220513

---
+++ Baekjoon 1075/Main_B2_1075.java
@@ -0,0 +1,17 @@
+package bronze;
+
+import java.util.*;
+
+public class Main_B2_1075 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt(), f=sc.nextInt();
+		n/=100;
+		for(int i=0;i<100;i++)
+			if((n*100+i)%f==0) {
+				System.out.printf("%02d",i);
+				break;
+			}
+	}
+	
+}
