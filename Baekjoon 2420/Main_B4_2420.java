6dd26a73a80106744105f181fcec16d81a4dfb35
hunucho <hunu_cho@naver.com>
2022-05-22 22:40:54 +0900

    20220522

---
+++ Baekjoon 2420/Main_B4_2420.java
@@ -0,0 +1,11 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Main_B4_2420 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n=sc.nextLong(),m=sc.nextLong();
+		System.out.println(Math.abs(n-m));
+	}
+}
