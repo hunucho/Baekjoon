8235e5133549b0fce1f484110fb39de5a1417793
hunucho <hunu_cho@naver.com>
2022-05-31 17:44:39 +0900

    20220531

---
+++ Baekjoon 9550/Main_B3_9550.java
@@ -0,0 +1,17 @@
+package bronze;
+
+import java.util.*;
+
+public class Main_B3_9550 {
+	public static void main(String[] args) {
+		Scanner sc  = new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int t=0;t<T;t++) {
+			int n=sc.nextInt(), k=sc.nextInt(), ans=0;
+			for(int i=0;i<n;i++)
+				ans+=sc.nextInt()/k;
+			System.out.println(ans);
+		}
+	}
+	
+}
