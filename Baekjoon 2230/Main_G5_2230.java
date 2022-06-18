56122708cd185a1f0c24452b05e0182f7d2e9507
hunucho <hunu_cho@naver.com>
2022-06-18 14:01:49 +0900

    20220618

---
+++ Baekjoon 2230/Main_G5_2230.java
@@ -0,0 +1,26 @@
+package gold;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_G5_2230 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n=Integer.parseInt(st.nextToken()), m=Integer.parseInt(st.nextToken());
+		int [] arr = new int [n];
+		for(int i=0;i<n;i++)
+			arr[i]=Integer.parseInt(br.readLine());
+		Arrays.sort(arr);
+		int s=0,e=0,ans=Integer.MAX_VALUE;
+		while(s<=e && e<n) {
+			if(arr[e]-arr[s]>=m) {
+				ans=Math.min(ans, arr[e]-arr[s]);
+				s++;
+			}
+			else
+				e++;
+		}
+		System.out.println(ans);
+	}
+}
