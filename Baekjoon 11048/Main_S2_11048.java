f241feb0bab292acd49c3fbe50a26e0e6e625144
hunucho <hunu_cho@naver.com>
2022-06-29 17:21:45 +0900

    20220629

--- Baekjoon 11048/Main_S2_11048.java
+++ Baekjoon 11048/Main_S2_11048.java
@@ -8,16 +8,11 @@ public class Main_S2_11048 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n=Integer.parseInt(st.nextToken()), m=Integer.parseInt(st.nextToken());
-		int [][] arr = new int [n+2][m+2], dp = new int[n+2][m+2];
+		int [][] dp = new int[n+2][m+2];
 		for(int i=1;i<=n;i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=1;j<=m;j++)
-				arr[i][j]=Integer.parseInt(st.nextToken());
-		}
-		
-		for(int i=1;i<=n;i++) {
-			for(int j=1;j<=m;j++)
-				dp[i][j]=Math.max(dp[i-1][j]+arr[i][j], dp[i][j-1]+arr[i][j]);
+				dp[i][j]=Math.max(dp[i-1][j], dp[i][j-1])+Integer.parseInt(st.nextToken());
 		}
 		System.out.println(dp[n][m]);		
 	}
