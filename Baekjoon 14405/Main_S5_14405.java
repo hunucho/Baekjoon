2d31d88c73d4a2a73ae017666e6dbdc2da709604
hunucho <hunu_cho@naver.com>
2022-07-12 23:55:15 +0900

    20220712

---
+++ Baekjoon 14405/Main_S5_14405.java
@@ -0,0 +1,10 @@
+package silver;
+
+import java.util.*;
+
+public class Main_S5_14405 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextLine().replace("pi"," ").replace("ka"," ").replace("chu"," ").replace(" ","").length()>0?"NO":"YES");
+	}
+}
