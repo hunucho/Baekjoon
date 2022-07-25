8fb081ba1fe822669e077375f4ff17085d52dcc8
hunucho <hunu_cho@naver.com>
2022-07-25 22:18:36 +0900

    20220725

---
+++ Baekjoon 8595/Main_B1_8595.java
@@ -0,0 +1,16 @@
+package bronze;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_B1_8595 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+		long ans=0;
+		StringTokenizer st = new StringTokenizer(br.readLine().replaceAll("[a-zA-Z]", " "));
+		while(st.hasMoreTokens())
+			ans+=Integer.parseInt(st.nextToken());
+		System.out.println(ans);
+	}
+}
