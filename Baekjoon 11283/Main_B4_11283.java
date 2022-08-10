17832127cdd225a46958dde67b197fe0038b0e36
hunucho <hunu_cho@naver.com>
2022-08-10 22:38:42 +0900

    20220810

---
+++ Baekjoon 11283/Main_B4_11283.java
@@ -0,0 +1,10 @@
+package bronze;
+
+import java.util.*;
+
+public class Main_B4_11283 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextLine().charAt(0)-'가'+1);
+	}
+}
