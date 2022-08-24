ad96b036c9c0a52879a28a48e59fae0ffcfeb800
hunucho <hunu_cho@naver.com>
2022-08-24 22:03:16 +0900

    20220824

---
+++ Baekjoon 24082/Main_B5_24082.java
@@ -0,0 +1,10 @@
+package bronze;
+
+import java.util.*;
+
+public class Main_B5_24082 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println((int)Math.pow(sc.nextInt(),3));
+	}
+}
