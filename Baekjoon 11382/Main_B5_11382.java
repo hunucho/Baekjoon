5ec1660aaee34f76ee0e775d41fdb3cc50822ca5
hunucho <hunu_cho@naver.com>
2022-08-29 22:57:42 +0900

    20220829

---
+++ Baekjoon 11382/Main_B5_11382.java
@@ -0,0 +1,11 @@
+package bronze;
+
+import java.util.*;
+
+public class Main_B5_11382 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long a=sc.nextLong(), b=sc.nextLong(), c=sc.nextLong();
+		System.out.println(a+b+c);
+	}
+}
