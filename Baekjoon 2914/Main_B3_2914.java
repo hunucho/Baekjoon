a1e30639bd1dd937d71634edf5d6300726d102c0
hunucho <hunu_cho@naver.com>
2022-09-08 20:53:02 +0900

    20220908

---
+++ Baekjoon 2914/Main_B3_2914.java
@@ -0,0 +1,11 @@
+package bronze;
+
+import java.util.*;
+
+public class Main_B3_2914 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt(), l=sc.nextInt()-1;
+		System.out.println(a*l+1);
+	}
+}
