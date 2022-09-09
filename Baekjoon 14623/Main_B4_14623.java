06874b89b8d78c5715deb0936c7f5890228910f7
hunucho <hunu_cho@naver.com>
2022-09-10 00:27:36 +0900

    20220910

---
+++ Baekjoon 14623/Main_B4_14623.java
@@ -0,0 +1,11 @@
+package bronze;
+
+import java.util.*;
+
+public class Main_B4_14623 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long a=sc.nextLong(2),b=sc.nextLong(2);
+		System.out.println(Long.toString(a*b, 2));
+	}
+}
