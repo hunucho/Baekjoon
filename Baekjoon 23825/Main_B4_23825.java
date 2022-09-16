4e007a24104a4bcbe10f195146e4905312fe5d9a
hunucho <hunu_cho@naver.com>
2022-09-17 08:28:44 +0900

    20220917

---
+++ Baekjoon 23825/Main_B4_23825.java
@@ -0,0 +1,11 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Main_B4_23825 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt(), m=sc.nextInt();
+		System.out.println(Math.min(n/2, m/2));
+	}
+}
