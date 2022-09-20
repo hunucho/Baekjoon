b279e5f306d61ab337a17e101a406aa1729424c4
Hunwoo Cho <hunu_cho@naver.com>
2022-09-21 08:34:31 +0900

    20220921

---
+++ Baekjoon 2163/Main_B1_2163.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main_B1_2163 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt(), m=sc.nextInt();
+		System.out.println(n*m-1);
+	}
+}
