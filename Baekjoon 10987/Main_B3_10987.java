40da0f8830afd507a88f69c8c25b9da6bc859f92
Hunwoo Cho <hunu_cho@naver.com>
2022-10-06 08:54:33 +0900

    20221006

---
+++ Baekjoon 10987/Main_B3_10987.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class Main_B3_10987 {
+	public static void main(String[] args) {
+		System.out.println(new Scanner(System.in).nextLine().replaceAll("[^aeiou]","").length());
+	}
+}
