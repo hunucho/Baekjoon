96bd510391645992acc4b7d40c9275c859687ab6
Hunwoo Cho <hunu_cho@naver.com>
2022-10-12 08:39:16 +0900

    20221012

---
+++ Baekjoon 25628/Main_B4_25628.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main_B4_25628 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt(), b=sc.nextInt();
+		System.out.println(Math.min(a/2, b));
+	}
+}
