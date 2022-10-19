c4c53153ad72b9bbaf6ba899c937a5e0570b0372
hunucho <hunu_cho@naver.com>
2022-10-19 21:16:23 +0900

    20221019

---
+++ Baekjoon 9295/Main_B3_9295.java
@@ -0,0 +1,12 @@
+package bronze;
+
+import java.util.*;
+
+public class Main_B3_9295 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int x=1;x<=T;x++)
+			System.out.printf("Case %d: %d\n",x, sc.nextInt()+sc.nextInt());
+	}
+}
