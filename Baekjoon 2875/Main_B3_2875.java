9cf97b7107c128711ce777f80e9de1e417f86430
Hunwoo Cho <hunu_cho@naver.com>
2022-11-11 08:33:30 +0900

    20221111

---
+++ Baekjoon 2875/Main_B3_2875.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt(), m=sc.nextInt(), k=sc.nextInt();
+		System.out.println(Math.min((n+m-k)/3, Math.min(m, n/2)));
+	}
+}
