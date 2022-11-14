978b8933db13222f5d01df368bc1b8dcb16dbf8e
hunucho <hunu_cho@naver.com>
2022-11-14 21:09:37 +0900

    20221114

---
+++ Baekjoon 24723/Main_B4_24723.java
@@ -0,0 +1,9 @@
+package bronze;
+
+import java.util.*;
+
+public class Main_B4_24723 {
+	public static void main(String[] args) {
+		System.out.println(Math.round(Math.pow(2, new Scanner(System.in).nextInt())));
+	}
+}
