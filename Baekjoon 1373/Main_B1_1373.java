9a966519db0c1210259c27fbf3e2578d59966eb3
Hunwoo Cho <hunu_cho@naver.com>
2022-12-28 08:40:43 +0900

    20221228

---
+++ Baekjoon 1373/Main_B1_1373.java
@@ -0,0 +1,8 @@
+import java.math.*;
+import java.util.*;
+
+public class Main_B1_1373 {
+	public static void main(String[] args) {
+		System.out.println(new BigInteger(new Scanner(System.in).nextLine(),2).toString(8));
+	}
+}
