82829ae92cd56ab25287c01247ba0826a1ae99a6
Hunwoo Cho <hunu_cho@naver.com>
2023-01-02 09:45:51 +0900

    20230102

---
+++ Baekjoon 10996/Main_B2_10996.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main_B2_10996 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=0;i<2*n;i++) {
+			for(int j=0;j<n;j++)
+				System.out.print((i+j)%2==0?"*":" ");
+			System.out.println();
+		}
+	}
+}
