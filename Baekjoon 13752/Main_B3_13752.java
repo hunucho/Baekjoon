75047a8f894c35961515ecf8fe1210bc09754c06
hunucho <hunu_cho@naver.com>
2023-02-10 21:55:01 +0900

    20230210

---
+++ Baekjoon 13752/Main_B3_13752.java
@@ -0,0 +1,12 @@
+package bronze;
+
+import java.util.*;
+
+public class Main_B3_13752 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++)
+			System.out.println("=".repeat(sc.nextInt()));
+	}
+}
