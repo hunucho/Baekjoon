59c4620a7128ee52f614e23714487026e768720d
hunucho <hunu_cho@naver.com>
2023-02-25 10:18:00 +0900

    20230225

---
+++ Baekjoon 15680/Main_B4_15680.java
@@ -0,0 +1,7 @@
+package bronze;
+
+public class Main_B4_15680 {
+	public static void main(String[] args) {		
+		System.out.println(new java.util.Scanner(System.in).nextInt()==0?"YONSEI":"Leading the Way to the Future");		
+	}
+}
