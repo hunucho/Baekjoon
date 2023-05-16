9d23f973ec4f5849969f255ae7f0e98e1eeaed70
Hunwoo Cho <hunu_cho@naver.com>
2023-05-16 18:35:21 +0900

    20230516

---
+++ Baekjoon 2810/Main_B1_2810.java
@@ -0,0 +1,10 @@
+package bronze;
+
+import java.util.*;
+
+public class Main_B1_2810 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(Math.min(sc.nextInt(), sc.next().replace("LL", "L").length()+1));			
+	}
+}
