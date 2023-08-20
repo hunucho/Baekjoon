9c5eb077decbe212762a603f8afb0d934a4401b1
Hunwoo Cho <hunu_cho@naver.com>
2023-08-20 09:13:25 +0200

    20280820

---
+++ Baekjoon 4766/Main_B3_4766.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double prev = sc.nextDouble();
+        while(true){
+            double cur=sc.nextDouble();
+            if(cur==999)
+                break;
+            System.out.printf("%.2f\n",cur-prev);
+            prev=cur;
+        }
+    }
+}
