d4962db50f6c9416dc3b69bd07ce783202fc1acc
Hunwoo Cho <hunu_cho@naver.com>
2023-08-29 12:11:00 +0200

    20230829

---
+++ Baekjoon 11024/Main_B3_11024.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T=Integer.parseInt(br.readLine());
+        while(T>0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int ans=0;
+            while(st.hasMoreTokens())
+                ans+=Integer.parseInt(st.nextToken());
+            System.out.println(ans);
+            T--;
+        }
+    }
+}
