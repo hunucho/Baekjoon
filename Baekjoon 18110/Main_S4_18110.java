3b63e538b17f700a4629f638c7eb7e8ee549bfd5
Hunwoo Cho <hunu_cho@naver.com>
2023-09-18 21:50:22 +0900

    20230918

--- Baekjoon 18110/Main_S4_18110.java
+++ Baekjoon 18110/Main_S4_18110.java
@@ -1,25 +1,18 @@
-package silver;
-
 import java.io.*;
 import java.util.*;
 
-public class Main_S4_18110 {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n=Integer.parseInt(br.readLine()), m=(int)Math.ceil(n*0.15), ans=0;
-		int [] arr = new int [n];
-		for(int i=0;i<n;i++)
-			arr[i]=Integer.parseInt(br.readLine());
-		Arrays.sort(arr);
-		if(n<=3) {
-			for(int i=0;i<n;i++)
-				ans+=arr[i];
-			ans=(int)Math.round(1.0*ans/n);
-		} else {
-			for(int i=m;i<n-m;i++)
-				ans+=arr[i];
-			ans=(int)Math.round(1.0*ans/(n-2*m));
-		}
-		System.out.println(ans);
-	}
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine()), cnt= (int) Math.round((double)N * 0.15), sum=0, div=N-2*cnt;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i=0;i<N;i++)
+            pq.add(Integer.parseInt(br.readLine()));
+        for(int i=0;i<cnt;i++)
+            pq.poll();
+        for(int i=0;i<N-2*cnt;i++)
+            sum += pq.poll();
+        System.out.printf("%d",div<=0?0:Math.round((double)(sum)/div));
+    }
 }
