f8d3c49cde298b1b602dfdd8ab68faabc96235b8
Hunwoo Cho <hunu_cho@naver.com>
2023-10-11 22:31:35 +0900

    20231011

--- Baekjoon 11723/Main_S5_11723.java
+++ Baekjoon 11723/Main_S5_11723.java
@@ -1,41 +1,30 @@
-package silver;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-
-public class Main_S5_11723 {
-
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int m = Integer.parseInt(br.readLine());
-		boolean [] arr = new boolean [21], brr = new boolean[21], crr = new boolean[21];
-		for(int i=1;i<=20;i++)
-			brr[i]=true;
-		for(int i=0;i<m;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String tmp=st.nextToken();
-			if(tmp.equals("all")) {
-				arr=brr;
-				continue;
-			}
-			else if(tmp.equals("empty")) {
-				arr=crr;
-				continue;
-			}
-			int val=Integer.parseInt(st.nextToken());
-			if(tmp.equals("add"))
-				arr[val]=true;
-			else if(tmp.equals("remove"))
-				arr[val]=false;
-			else if(tmp.equals("check"))
-				bw.write(arr[val]?"1\n":"0\n");
-			else if(tmp.equals("toggle"))
-				arr[val]=!arr[val];
-		}
-		bw.close();
-	}
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N=Integer.parseInt(br.readLine());
+        boolean [] S = new boolean[21];
+        for(int i=0;i<N;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String oper = st.nextToken();
+            if(oper.equals("all"))
+                Arrays.fill(S,true);
+            else if (oper.equals("empty"))
+                Arrays.fill(S,false);
+            else {
+                int x = Integer.parseInt(st.nextToken());
+                if(oper.equals("add"))
+                    S[x]=true;
+                else if(oper.equals("remove"))
+                    S[x]=false;
+                else if(oper.equals("check"))
+                    bw.write(S[x]?"1\n":"0\n");
+                else if(oper.equals("toggle"))
+                    S[x]=!S[x];
+            }
+        }
+        bw.close();
+    }
 }
