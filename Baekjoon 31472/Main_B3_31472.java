53ca0682a56b6db9985619b909291df131b796f1
Hunwoo Cho <hunu_cho@naver.com>
2024-03-04 16:17:15 +0900

    20240304

---
+++ Baekjoon 31472/Main_B3_31472.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(8*(int)Math.sqrt(sc.nextInt()/2));
+    }
+}
