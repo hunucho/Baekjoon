e6d0693d33a6b4279b744209c57eeb860fc04284
Hunwoo Cho <hunu_cho@naver.com>
2024-05-21 17:28:29 +0900

    20240521

---
+++ Baekjoon 4806/Main_B3_4806.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int ans=0;
+        while(sc.hasNext()) {
+        	sc.nextLine();
+        	ans++;
+        }
+        System.out.println(ans);
+    }
+}
+
