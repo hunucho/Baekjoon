0fef29949db4955d744695686dd7f51f3dca2414
Hunwoo Cho <hunu_cho@naver.com>
2024-06-21 17:17:18 +0900

    20240621

---
+++ Baekjoon 15740/Main_B5_15740.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		BigInteger A = new BigInteger(sc.next());
+		System.out.println(A.add(new BigInteger(sc.next())));
+	}	
+}
