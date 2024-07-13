2c99dacc0fe3c67bee499fd4a6732779e4fadcd6
Hunwoo Cho <hunu_cho@naver.com>
2024-07-13 21:54:39 +0900

    20240713

---
+++ Baekjoon 26082/Main_B5_26082.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int A=sc.nextInt(), B=sc.nextInt(), C=sc.nextInt();
+        System.out.println(B/A*C*3);
+    }
+}
