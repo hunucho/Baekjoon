c83e91accdf32997c6e88d51a7cce758f09c22cb
Hunwoo Cho <hunu_cho@naver.com>
2024-07-21 00:10:39 +0900

    20240721

---
+++ Baekjoon 31780/Main_S5_31780.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int X=sc.nextInt(), M=sc.nextInt();
+        System.out.println(X*(M+1));
+    }
+}
