3d1ac9d6105b16cc91a7bf1a90679a63a0945b0e
Hunwoo Cho <hunu_cho@naver.com>
2025-03-06 21:25:23 +0900

    20250306

---
+++ 2025/Main_B5_33515.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Math.min(sc.nextInt(), sc.nextInt()));
+    }
+}
