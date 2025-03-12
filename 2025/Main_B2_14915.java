a281e2a199a810999a1dee08a28715ac409af07f
Hunwoo Cho <hunu_cho@naver.com>
2025-03-13 00:17:39 +0900

    20250313

---
+++ 2025/Main_B2_14915.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int [][] arr;
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Integer.toString(sc.nextInt(), sc.nextInt()).toUpperCase());
+    }
+}
