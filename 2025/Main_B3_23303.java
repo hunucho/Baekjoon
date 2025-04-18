1afa9f315bb47ce4002623875529e543fd22d903
Hunwoo Cho <hunu_cho@naver.com>
2025-04-18 22:33:11 +0900

    20250418

---
+++ 2025/Main_B3_23303.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        System.out.println(S.toLowerCase().contains("d2")?"D2":"unrated");
+    }
+}
