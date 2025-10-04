8c975bf7e50c9cde4a1118d718520dddeabedae4
Hunwoo Cho <hunu_cho@naver.com>
2025-10-04 18:26:10 +0900

    20251004

---
+++ 2025/Main_B2_14491.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T=Integer.parseInt(br.readLine());
+        System.out.print(Integer.toString(T, 9));
+    }
+}
