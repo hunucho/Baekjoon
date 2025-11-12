f671483915a1e6af45f4bde4e9471763501078a3
Hunwoo Cho <hunu_cho@naver.com>
2025-11-13 01:27:18 +0900

    20251113

---
+++ 2025/Main_B5_34721.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.print("I love DGU\n".repeat(N));
+    }
+}
