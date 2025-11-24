99414be41e4c455086b30e299b5874c1de2db09d
Hunwoo Cho <hunu_cho@naver.com>
2025-11-25 02:15:28 +0900

    20251125

---
+++ 2025/Main_B4_32651.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
+        System.out.print(N<=100000&&N%2024==0?"Yes":"No");
+    }
+}
