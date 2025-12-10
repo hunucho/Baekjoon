fdf3a4838995c459e786a0f1c2f79a1de6dc598a
Hunwoo Cho <hunu_cho@naver.com>
2025-12-11 00:27:25 +0900

    20251211

---
+++ 2025/Main_B5_34665.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(br.readLine().equals(br.readLine())==true?0:1550);
+    }
+}
