744870eb1b699abc13f4f569e5df6255bdafc4d6
Hunwoo Cho <hunu_cho@naver.com>
2025-12-25 20:46:39 +0100

    20251225

---
+++ 2025/Main_B5_34945.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.print(x>=6?"Success!":"Oh My God!");
+    }
+}
