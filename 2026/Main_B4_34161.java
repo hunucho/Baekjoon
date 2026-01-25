f672a95ef75c507da5fc8ba9a435f9c6139ebb25
Hunwoo Cho <hunu_cho@naver.com>
2026-01-26 00:28:17 +0900

    20260122

---
+++ 2026/Main_B4_34161.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<10000;i++)
+            sb.append(-1).append("\n");
+        System.out.print(sb.toString());
+    }
+}
